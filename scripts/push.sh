56b0984f7935ec9c9f489ae1b5e62eecce51a66f
xxradar <git-xxradar@radarhack.com>
2019-05-22 11:24:06 +0200

    Fixed JENKINSFILE

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,6 +2,6 @@
 echo  "*** Login to Dockerhub ***"
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 echo  "*** TAG image to xxradar repo ***"
-docker tag fqdnsan_scan:$BUILD_VAR xxradar/fqdnsan_scan:$BUILD_VAR
+docker tag fqdnsan_scan:$BUILD_ID xxradar/fqdnsan_scan:$BUILD_ID
 echo  "*** Push image ***"
-docker push xxradar/fqdnsan_scan:$BUILD_VAR
+docker push xxradar/fqdnsan_scan:$BUILD_ID
